2ea53648f3748046c17c3e7e593246a39e0aa258
Yihan Huang <huangehan@gmail.com>
2025-03-11 20:38:29 +1100

    fix: switch to weatherapi

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,47 +1,76 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { Loader } from '@googlemaps/js-api-loader';
-import { convertHeatData } from '@/util/convertHeatData'
 
-const message = ref('Here is all you need to know.')
+const API_KEY = "0f2ce572252f4ef9ba455535251103"
+
+let map = ref(null);
+
+async function fetchUV(lat, lng) {
+  if(!lat || !lng) return null;
+    try {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${lat},${lng}&aqi=yes`);
+    const data = await response.json();
+    if (data && data["current"]["uv"] !== undefined) {
+      return data["current"]["uv"];
+    }
+  } catch (error) {
+    console.error("Error fetching UV data:", error);
+  }
+  return null;
+}
+
 const loader = new Loader({
   apiKey: "AIzaSyDfAbRiEWwIZa32G7UGOkVfMiwzoFwR_eQ",
   version: "weekly",
   libraries: ["visualization"]
 });
 
+const userLat = ref(null);
+const userLng = ref(null);
 const mapOptions = {
   center: {
-    lat: -37.9145,
-    lng: 145.1275
+    lat: userLat.value? userLat.value : -37.9145,
+    lng: userLng.value? userLng.value : 145.1275
   },
-  zoom: 10
+  zoom: 12
 };
 
 
-let map = ref(null);
+
+const userUV = ref(null)
+onMounted(async ()=> {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        userLat.value = position.coords.latitude;
+        userLng.value = position.coords.longitude;
+      }
+    )
+  }
+
+  if(userLat.value && userLng.value) {
+    userUV.value = await fetchUV(userLat.value, userLng.value)
+  }
+})
+
+
 loader.load()
   .then(async (google) => {
-    console.log(11)
-    var heatmapData = await convertHeatData(google.maps.LatLng)
-    console.log(heatmapData)
     map.value = new google.maps.Map(document.getElementById("map"), mapOptions);
-    var heatmap = new google.maps.visualization.HeatmapLayer({
-      data: heatmapData
-    });
-    heatmap.setMap(map.value);
   })
   .catch(e => {
     console.log(e)
   });
-
 </script>
 
 <template>
   <div>
     <h1>Skin Protection</h1>
-    <p>{{ message }}</p>
+    <div class="d-flex p-2">
     <div id="map" style="width: 500px; height:500px"></div>
+    <div>user UV: {{ userUV }}</div>
+  </div>
   </div>
 
 </template>
