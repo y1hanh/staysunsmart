fd5d3fae7775ac9488727af53957d17d92a5a77c
jiaying <jlin0158@student.monash.edu>
2025-03-11 15:45:33 +1100

    add clothing button

--- src/router/router.js
+++ src/router/router.js
@@ -3,12 +3,14 @@ import homePage from '../views/HomePage.vue'
 import impact from '../views/Impact.vue'
 import personalisation from '../views/Personalisation.vue'
 import uvindex from '../views/UVIndex.vue'
+import clothing from '../views/Clothing.vue'
 
 const routes = [
   { path: '/', component: homePage },
   { path: '/impact', component: impact },
   { path: '/personalisation', component: personalisation },
   { path: '/uvindex', component: uvindex },
+  {path: '/clothing', component: clothing}
 ]
 
 const router = createRouter({
---
+++ src/views/Clothing.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <div class="page-content"> 
+        <p> Here is a blog suggesting clothing to help you conquer high UV exposure.</p>
+        <a href="https://www.skincancer.org/skin-cancer-prevention/sun-protection/sun-protective-clothing/">
+            Click here to read more.
+        </a>
+    </div>
+    </template>
