a960df1f87433c5b9e7f79c3bc33e11662d0fd0e
jiaying <jlin0158@student.monash.edu>
2025-03-11 19:52:34 +1100

    subscribe.vue

--- src/router/router.js
+++ src/router/router.js
@@ -4,13 +4,15 @@ import impact from '../views/Impact.vue'
 import personalisation from '../views/Personalisation.vue'
 import uvindex from '../views/UVIndex.vue'
 import clothing from '../views/Clothing.vue'
+import Subscribe from '../views/Subscribe.vue'
 
 const routes = [
   { path: '/', component: homePage },
   { path: '/impact', component: impact },
   { path: '/personalisation', component: personalisation },
   { path: '/uvindex', component: uvindex },
-  {path: '/clothing', component: clothing}
+  {path: '/clothing', component: clothing},
+  {path: '/subscribe', component: Subscribe}
 ]
 
 const router = createRouter({
